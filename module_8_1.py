e9bceaf7afb01371c6841809e93d03867560bff4
ferstapen <164868955+ferstapen@users.noreply.github.com>
2024-11-06 12:23:09 +0500

    Create module_8_1.py

---
+++ module_8_1.py
@@ -0,0 +1,9 @@
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3)
+    except TypeError as exc:
+        return f'{a}{b}'
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
